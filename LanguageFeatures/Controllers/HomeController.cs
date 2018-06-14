fd52c69be5dd01b9cc7f2a8e0785e71fe73f4def
Підвашецький Денис <pod.den1804@gmail.com>
2018-06-15 00:37:11 +0300

    Add Files for start to use C# features

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+            foreach (Product product in Product.GetProducts())
+            {
+                string name = product?.Name;
+                decimal? price = product?.Price;
+                results.Add(String.Format("Name: {0}, Price: {1}", name, price));
+            }
+            return View(results);
+        }
+    }
+}
