8a219631f47da3ca90685e1e62b25358bdeed650
Denis Pidavashetskyi <pod.den1804@gmail.com>
2018-06-15 11:52:13 +0300

    Фишки с авто свойствами

--- LanguageFeatures/Models/Products.cs
+++ LanguageFeatures/Models/Products.cs
@@ -8,15 +8,21 @@ namespace LanguageFeatures.Models
     public class Product
     {
         public string Name { get; set; }
+        // Using Auto-Implemented Property Initializers
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        // Creating Read-Only Automatically Implemented Properties
+        // Данное свойство не возможно изменить, разве что только в конструкторе
+        public bool InStock { get; } = true;  
 
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
                 Name = "Kayak",
-                Price = 275M
+                Price = 275M, 
+                Category = "Water Craft"
             };
             Product lifejacket = new Product
             {
